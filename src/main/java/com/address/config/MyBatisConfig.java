1cce14cf54afdbb413ace2639abd70f68e122202
huangxg8493 <994702611@qq.com>
2026-04-21 14:09:24 +0800

    refactor: MyBatisConfig 使用 import + Logger
    - Configuration/Environment/XMLMapperBuilder 改为 import 方式
    - System.out/System.err 改为 Logger.info/Logger.error

--- src/main/java/com/address/config/MyBatisConfig.java
+++ src/main/java/com/address/config/MyBatisConfig.java
@@ -1,23 +1,29 @@
 package com.address.config;
 
 import com.address.model.CifAddress;
+import org.apache.ibatis.builder.xml.XMLMapperBuilder;
 import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.mapping.Configuration;
+import org.apache.ibatis.mapping.Environment;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.apache.ibatis.transaction.jdbc.JdbcTransactionFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.InputStream;
 
 public class MyBatisConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyBatisConfig.class);
     private static final SqlSessionFactory SQL_SESSION_FACTORY;
 
     static {
         try {
-            org.apache.ibatis.session.Configuration config = new org.apache.ibatis.session.Configuration();
+            Configuration config = new Configuration();
 
             config.setEnvironment(
-                new org.apache.ibatis.mapping.Environment(
+                new Environment(
                     "development",
                     new JdbcTransactionFactory(),
                     DbConfig.getDataSource()
@@ -27,15 +33,15 @@ public class MyBatisConfig {
             config.getTypeAliasRegistry().registerAlias("CifAddress", CifAddress.class);
 
             InputStream mapperInputStream = Resources.getResourceAsStream("mapper/CifAddressMapper.xml");
-            org.apache.ibatis.builder.xml.XMLMapperBuilder mapperBuilder =
-                new org.apache.ibatis.builder.xml.XMLMapperBuilder(
+            XMLMapperBuilder mapperBuilder =
+                new XMLMapperBuilder(
                     mapperInputStream, config, "mapper/CifAddressMapper.xml", config.getSqlFragments());
             mapperBuilder.parse();
 
             SQL_SESSION_FACTORY = new SqlSessionFactoryBuilder().build(config);
-            System.out.println("MyBatisConfig 初始化成功");
+            logger.info("MyBatisConfig 初始化成功");
         } catch (Exception e) {
-            System.err.println("MyBatisConfig 初始化失败: " + e.getMessage());
+            logger.error("MyBatisConfig 初始化失败: " + e.getMessage());
             e.printStackTrace();
             throw new RuntimeException("初始化 MyBatis SqlSessionFactory 失败", e);
         }
