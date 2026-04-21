c105ca5e7f79eb67bbe33b2e7347fafcd0aa87eb
huangxg8493 <994702611@qq.com>
2026-04-21 11:52:03 +0800

    feat: 添加 CifAddressMapper 接口

---
+++ src/main/java/com/address/repository/CifAddressMapper.java
@@ -0,0 +1,23 @@
+package com.address.repository;
+
+import com.address.model.CifAddress;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface CifAddressMapper {
+
+    @Update("UPDATE CIF_ADDRESS SET DEL_FLAG = 'Y' WHERE SEQ_NO = #{seqNo}")
+    void delete(@Param("seqNo") String seqNo);
+
+    List<CifAddress> findByClientNo(@Param("clientNo") String clientNo, @Param("delFlag") String delFlag);
+
+    void save(CifAddress address);
+
+    void update(CifAddress address);
+
+    void saveAll(List<CifAddress> addresses);
+
+    void updateAll(List<CifAddress> addresses);
+}
