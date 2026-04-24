9fd4a2e55e7ae576bb21089bac00135491eac0c9
huangxg8493 <994702611@qq.com>
2026-04-24 09:01:21 +0800

    feat: 添加认证模块错误码常量类 AuthErrorCode

---
+++ src/main/java/com/address/common/AuthErrorCode.java
@@ -0,0 +1,8 @@
+package com.address.common;
+
+public class AuthErrorCode {
+    public static final String SUCCESS = "000000";
+    public static final String USER_NOT_FOUND = "101001";
+    public static final String USER_DISABLED = "101002";
+    public static final String PASSWORD_ERROR = "101003";
+}
