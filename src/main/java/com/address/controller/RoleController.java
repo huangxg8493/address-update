dd9a50a730ab1b68430f518027f6dad48d218fbf
huangxg8493 <994702611@qq.com>
2026-04-27 17:50:09 +0800

    feat: RoleController 添加菜单分配和查询接口

--- src/main/java/com/address/controller/RoleController.java
+++ src/main/java/com/address/controller/RoleController.java
@@ -1,14 +1,18 @@
 package com.address.controller;
 
 import com.address.common.ApiResponse;
+import com.address.dto.MenuResponse;
 import com.address.dto.RoleCreateRequest;
 import com.address.dto.RoleQueryRequest;
 import com.address.dto.RoleResponse;
 import com.address.dto.RoleUpdateRequest;
+import com.address.model.SysMenu;
+import com.address.service.RoleMenuService;
 import com.address.service.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -16,6 +20,9 @@ public class RoleController {
 
     @Autowired
     private RoleService roleService;
+
+    @Autowired
+    private RoleMenuService roleMenuService;
 
     @PostMapping("/api/roles/query")
     public ApiResponse<List<RoleResponse>> query(@RequestBody RoleQueryRequest request) {
@@ -48,5 +55,27 @@ public class RoleController {
     public ApiResponse<Void> assignDataScopes(@PathVariable Long roleId, @RequestBody List<Long> scopeIds) {
         roleService.assignDataScopes(roleId, scopeIds);
         return ApiResponse.success(null);
+    }
+
+    @PostMapping("/api/roles/{roleId}/menus")
+    public ApiResponse<Void> assignMenus(@PathVariable Long roleId, @RequestBody List<Long> menuIds) {
+        roleMenuService.assignMenus(roleId, menuIds);
+        return ApiResponse.success(null);
+    }
+
+    @GetMapping("/api/roles/{roleId}/menus")
+    public ApiResponse<List<MenuResponse>> getMenus(@PathVariable Long roleId) {
+        List<SysMenu> menus = roleMenuService.getMenusByRoleId(roleId);
+        List<MenuResponse> responses = new ArrayList<>();
+        for (SysMenu menu : menus) {
+            MenuResponse r = new MenuResponse();
+            r.setMenuId(menu.getMenuId());
+            r.setMenuName(menu.getMenuName());
+            r.setMenuUrl(menu.getMenuUrl());
+            r.setIcon(menu.getIcon());
+            r.setParentId(menu.getParentId());
+            responses.add(r);
+        }
+        return ApiResponse.success(responses);
     }
 }
