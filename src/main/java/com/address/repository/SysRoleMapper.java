e589c1b367f6709e0b7dc7544b30f3fe86c462e1
huangxg8493 <994702611@qq.com>
2026-04-28 10:52:45 +0800

    fix: SysRoleMapper.findByIds 修复角色信息映射为 null 的问题

--- src/main/java/com/address/repository/SysRoleMapper.java
+++ src/main/java/com/address/repository/SysRoleMapper.java
@@ -21,7 +21,14 @@ public interface SysRoleMapper {
     @Select("SELECT * FROM SYS_ROLE")
     java.util.List<SysRole> findAll();
 
-    @Select("<script>SELECT * FROM SYS_ROLE WHERE role_id IN <foreach collection='roleIds' item='id' open='(' separator=',' close=')'>#{id}</foreach></script>")
+    @Select("<script>SELECT role_id, role_code, role_name, status, create_time FROM SYS_ROLE WHERE role_id IN <foreach collection='roleIds' item='id' open='(' separator=',' close=')'>#{id}</foreach></script>")
+    @Results({
+        @Result(property = "roleId", column = "role_id"),
+        @Result(property = "roleCode", column = "role_code"),
+        @Result(property = "roleName", column = "role_name"),
+        @Result(property = "status", column = "status"),
+        @Result(property = "createTime", column = "create_time")
+    })
     java.util.List<SysRole> findByIds(@Param("roleIds") java.util.List<Long> roleIds);
 
     @Delete("DELETE FROM SYS_ROLE WHERE role_id = #{roleId}")
