02588206ee752038e29b4066016fe1af77d056ea
huangxg8493 <994702611@qq.com>
2026-04-30 06:07:25 +0800

    feat: 创建密码重置请求 DTO
    新增 PasswordResetRequest.java，用于密码重置接口。

---
+++ src/main/java/com/address/dto/PasswordResetRequest.java
@@ -0,0 +1,8 @@
+package com.address.dto;
+
+public class PasswordResetRequest {
+    private String newPassword;
+
+    public String getNewPassword() { return newPassword; }
+    public void setNewPassword(String newPassword) { this.newPassword = newPassword; }
+}
